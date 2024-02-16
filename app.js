f6a8fe319ad6552dcdea36bf65dbfac86cbd1380
Broketoof25 <andrew.s.lall@gmail.com>
2024-02-15 23:00:44 -0500

    sessions 1

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -32,48 +34,57 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //cookieParser() w/secret key argument
-app.use(cookieParser('12345-67890-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
+//using sessions instead of cookieparser()
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 //add authenticaiton here, so user needs to authenticate before
 //accessing any data from server
 function auth(req, res, next) {
-    if (!req.signedCookies.user) {
-      const authHeader = req.headers.authorization;
-      if (!authHeader) {
+  console.log(req.session)
+  if (!req.session.user) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      const err = new Error('You are not authenticated!');
+      //below lets client know the server is requiring authentication
+      //and the authentication method is Basic.
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    const user = auth[0];
+    const pass = auth[1];
+    if (user === 'admin' && pass === 'password') {
+      //handles creating the cookie and setting it up in the server response to
+      //the client
+        req.session.user = 'admin';
+        return next(); // authorized
+    } else {
         const err = new Error('You are not authenticated!');
-        //below lets client know the server is requiring authentication
-        //and the authentication method is Basic.
-        res.setHeader('WWW-Authenticate', 'Basic');
+        res.setHeader('WWW-Authenticate', 'Basic');      
         err.status = 401;
         return next(err);
-      }
+    }
 
-      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-      const user = auth[0];
-      const pass = auth[1];
-      if (user === 'admin' && pass === 'password') {
-        //handles creating the cookie and setting it up in the server response to
-        //the client
-          res.cookie('user', 'admin', {signed: true})
-          return next(); // authorized
-      } else {
-          const err = new Error('You are not authenticated!');
-          res.setHeader('WWW-Authenticate', 'Basic');      
-          err.status = 401;
-          return next(err);
-      }
+} else {
+    if (req.session.user === 'admin') {
+      return next();
 
-  } else {
-      if (req.signedCookies.user === 'admin') {
-        return next();
+    } else {
+      const err = new Error('You are not authenticated!');    
+      err.status = 401;
+      return next(err);
+    }
 
-      } else {
-        const err = new Error('You are not authenticated!');    
-        err.status = 401;
-        return next(err);
-      }
-
-  }
+}
 }
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
